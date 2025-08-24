72c1869e93bd8b44c7d76a146d0e8b4d3ea4d35f
thamilyr <163945678+thamilyr@users.noreply.github.com>
2025-08-24 12:05:26 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,85 @@
+package adatech.ecommerce.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private int estoque;
+
+    public Produto(int id, String nome, String descricao, BigDecimal preco, int estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto {" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                ", estoque=" + estoque +
+                '}';
+    }
+
+        @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto)) return false;
+        Produto produto = (Produto) o;
+        return id == produto.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
+
---
+++ StatusPedido.java
@@ -0,0 +1,23 @@
+package adatech.ecommerce.model;
+
+public enum StatusPedido {
+    ABERTO("Pedido Aberto"),
+    AGUARDANDO_PAGAMENTO("Aguardando Pagamento"),
+    PAGO("Pago"),
+    FINALIZADO("Finalizado");
+
+    private final String descricao;
+
+    StatusPedido(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+}
