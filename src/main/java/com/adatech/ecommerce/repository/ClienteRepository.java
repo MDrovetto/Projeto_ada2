43174f0a6bf9feec1020db387b79522d2b887cca
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-08-31 19:02:19 -0300

    Atualização ClienteRepositoryImpl

--- src/main/java/com/adatech/ecommerce/repository/ClienteRepository.java
+++ src/main/java/com/adatech/ecommerce/repository/ClienteRepository.java
@@ -2,13 +2,7 @@ package main.java.com.adatech.ecommerce.repository;
 
 import main.java.com.adatech.ecommerce.model.Cliente;
 
-/**
- * Interface específica para o repositório de Clientes.
- * Herda as operações básicas de CrudRepository.
- * TODO:
- *  - Adicionar, se necessário, métodos específicos para busca de clientes,
- *    como buscarPorCpf(String cpf).
- */
+
 public interface ClienteRepository extends CrudRepository<Cliente, String> {
     Cliente buscarPorCpf(String cpf);
 }
--- src/main/java/com/adatech/ecommerce/repository/CrudRepository.java
+++ src/main/java/com/adatech/ecommerce/repository/CrudRepository.java
@@ -2,18 +2,8 @@ package main.java.com.adatech.ecommerce.repository;
 
 import java.util.List;
 
-/**
- * Interface genérica para operações de CRUD (Create, Read, Update, Delete).
- * @param <T> O tipo da entidade.
- * @param <ID> O tipo do ID da entidade.
- * TODO:
- *  - Definir os métodos:
- *    - salvar(T entidade)
- *    - listarTodos(): List<T>
- *    - buscarPorId(ID id): T
- */
 public interface CrudRepository<T, ID> {
-    void salvar(T entidade);
+    boolean salvar(T entidade);
     List<T> listarTodos();
     T buscarPorId(ID id);
 }
