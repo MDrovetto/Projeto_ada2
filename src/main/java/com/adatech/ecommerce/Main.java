3d04f308fbd8cd5f42cfd38f17b94a219988314b
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-10-02 12:11:24 -0300

    Corrigidos os packages

--- src/main/java/com/adatech/ecommerce/Main.java
+++ src/main/java/com/adatech/ecommerce/Main.java
@@ -3,7 +3,7 @@ package com.adatech.ecommerce;
 import com.adatech.ecommerce.view.MainView;
 
 public class Main {
-    public static void main(String[] args) {1
+    public static void main(String[] args) {
 
         MainView mainView = new MainView();
         mainView.iniciar();
